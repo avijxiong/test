ebaa6a67348fd5e227020a7ad5ae7413ff32cb2c
sadasdasasd <32637304+avijxxx@users.noreply.github.com>
2023-02-18 20:47:09 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,13 @@
 # crond
 /usr/sbin/crond -b -l 8
 #crontab/etc/init.d/cron start
+chmod +x /etc/mosdns/local -R
+chmod +x /etc/mosdns/remote -R
+chmod +x /etc/mosdns/rules/update*
+chmod +x /etc/mosdns/tools/adblock*
+chmod +x /etc/mosdns/tools/ecs*
+chmod +x /etc/mosdns/tools/ipv4*
+chmod +x /etc/mosdns/tools/socks5-reload
 /usr/bin/mosdns start --dir /etc/mosdns
 #tail -f /dev/null
 chmod +x /etc/mosdns/cache -R
